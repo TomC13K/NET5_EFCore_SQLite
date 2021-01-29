3e0d183360807e636c8e11c6df53893a844b3e39
TC <tomas.ciernik@coop.co.uk>
2021-01-29 00:24:25 +0000

    Initial check-in of module EFCoreWithSQLite

---
+++ EFCoreWithSQLite/ProductService.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EFCoreWithSQLite
+{
+    public static class ProductService
+    {
+        public static void Add(Product product)
+        {
+                using var context = new SampleContext();
+                var entity = context.Products.Add(product);
+                entity.State = EntityState.Added;
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                Console.WriteLine($"Error saving -> {ex.Message}");
+                //throw new Exception("Database error while trying to save changes: ", ex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {ex}");
+                //throw new Exception("Database general error while : ", ex);
+            }
+        }
+
+        public static void Edit(Product product)
+        {
+            using var context = new SampleContext();
+            var entity = context.Products.Update(product);
+            entity.State = EntityState.Modified;
+
+            context.SaveChanges();
+        }
+
+        public static void Delete(Product product)
+        {
+            using var context = new SampleContext();
+            var entity = context.Products.Remove(product);
+            entity.State = EntityState.Deleted;
+
+            context.SaveChanges();
+        }
+
+        public static Product Get(int productId)
+        {
+            using var context = new SampleContext();
+            var product = context.Products.Find(productId);
+            Console.WriteLine($"Product ID:{product.Id}; Product Name:{product.Name}");
+
+            return product;
+        }
+
+        public static void GetAll()
+        {
+            using var context = new SampleContext();
+            if(context.Products.Any())
+            {
+                var data = context.Products.ToList();
+                foreach (var product in data)
+                {
+                    Console.WriteLine($"Product ID:{product.Id}; Product Name:{product.Name}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are no records in the table");
+            }
+        }
+
+      
+    }
+}
---
+++ EFCoreWithSQLite/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EFCoreWithSQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">>> Starting the appppp <<<");
+
+            // create new object of type Product
+            Product product = new Product()
+            {
+                Id = 5,
+                Name = "Iteeeem 4"
+            };
+
+            //add it to the DB
+            ProductService.Add(product);
+
+            //get all products from the db
+            ProductService.GetAll();
+
+            //get the product from the db
+            Console.WriteLine("Original product: \n");
+            ProductService.Get(1);
+
+            //edit product
+            product.Name = "Newish laptop 3";
+            ProductService.Edit(product);
+
+            // get all products from DB
+            Console.WriteLine("List of all products: \n");
+            ProductService.GetAll();
+
+
+            //get the edited product from the db
+            Console.WriteLine("Edited product: \n");
+            ProductService.Get(4);
+        }
+    }
+}
